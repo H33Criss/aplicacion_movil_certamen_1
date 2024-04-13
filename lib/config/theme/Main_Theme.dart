9974aecfb59a1ee55f62a825ce4b77096b10386f
Cristobal-coding <agartkg95@gmail.com>
2024-04-13 17:30:29 -0400

    Cambiando la fuente de texto a nivel global

--- lib/config/theme/Main_Theme.dart
+++ lib/config/theme/Main_Theme.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 
 class MainTheme {
   ThemeData getTheme() => ThemeData(
+        fontFamily: 'Kanit',
         brightness: Brightness.dark,
         useMaterial3: true,
+        //Apartir del color yellow, se genera una paleta de colores
         colorSchemeSeed: Colors.yellow[600],
+
         // scaffoldBackgroundColor: Colors.red[900]?.withOpacity(0.15),
       );
 }
